09b9adfffecb7d1eda345e826701a29aea728c7b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-12-01 00:06:19 -0200

    Append migrations to core application.

--- lib/aranha/engine.rb
+++ lib/aranha/engine.rb
@@ -1,5 +1,11 @@
 module Aranha
   class Engine < ::Rails::Engine
     isolate_namespace Aranha
+
+    initializer :append_migrations do |app|
+      config.paths['db/migrate'].expanded.each do |expanded_path|
+        app.config.paths['db/migrate'] << expanded_path
+      end
+    end
   end
 end
