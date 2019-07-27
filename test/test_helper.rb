e5a532f7d47409cab62ca2ec2980d5729cfe8922
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-07-27 15:14:05 -0300

    Move "test/dummy" to "spec/support/rails_app".

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,9 @@
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../test/dummy/config/environment.rb', __dir__)
+require File.expand_path('../spec/support/rails_apps/config/environment.rb', __dir__)
 ActiveRecord::Migrator.migrations_paths = [
-  File.expand_path('../test/dummy/db/migrate', __dir__)
+  File.expand_path('../spec/support/rails_app/db/migrate', __dir__)
 ]
 require 'rails/test_help'
 
