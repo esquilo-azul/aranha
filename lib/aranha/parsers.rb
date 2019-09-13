caba654600d5a706aa564cba9ad9c717aca11b03
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-12 23:41:00 -0300

    Replace "lib/aranha/parsers" from self by gem aranha-parsers'.

--- lib/aranha/parsers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Aranha
-  module Parsers
-    require 'aranha/parsers/base'
-    require 'aranha/parsers/html'
-    require 'aranha/parsers/invalid_state_exception'
-    require 'aranha/parsers/source_address'
-  end
-end
