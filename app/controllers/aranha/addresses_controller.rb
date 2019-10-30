a75e2741745a0ba91706ad03eb00db3b78829f79
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-30 15:57:55 -0300

    Aranha::ApplicationController: remove.

--- app/controllers/aranha/addresses_controller.rb
+++ app/controllers/aranha/addresses_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-require_dependency 'aranha/application_controller'
-
 module Aranha
-  class AddressesController < ApplicationController
+  class AddressesController < ::ApplicationController
     active_scaffold :'aranha/address' do |_conf|
     end
   end
