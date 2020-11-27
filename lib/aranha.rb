5877618db739beadce461ff48622dd19964ac8d8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-27 12:02:26 -0300

    Aranha: remove unused requires.

--- lib/aranha.rb
+++ lib/aranha.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require 'httpclient'
-require 'active_support/dependencies'
-require 'active_scaffold'
-
 module Aranha
   require 'aranha/default_processor'
   require 'aranha/dom_elements_traverser'
