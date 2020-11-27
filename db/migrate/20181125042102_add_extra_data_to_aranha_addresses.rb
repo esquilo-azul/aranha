374c210126b0540f06b133465f298c4c535542c0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-27 17:46:48 -0300

    Aranha::Address: replaced by Aranha::Address in gem "aranha-rails".

--- db/migrate/20181125042102_add_extra_data_to_aranha_addresses.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddExtraDataToAranhaAddresses < (
-    Rails.version < '5' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
-  )
-  def change
-    add_column :aranha_addresses, :extra_data, :text
-  end
-end
