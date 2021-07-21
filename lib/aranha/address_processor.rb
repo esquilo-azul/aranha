4b730145902dfc4124809331dc08f0b6f963f45a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-21 14:16:44 -0300

    Aranha::AddressProcessor: extract from Aranha::Processor.

---
+++ lib/aranha/address_processor.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Aranha
+  class AddressProcessor
+    ARANHA_EXCEPTIONS = [::Aranha::Parsers::InvalidStateException].freeze
+    CORE_EXCEPTIONS = [::SocketError].freeze
+    ERRNO_EXCEPTIONS = [Errno::ECONNREFUSED, ::Errno::ECONNRESET].freeze
+    HTTPCLIENT_EXCEPTIONS = [
+      ::HTTPClient::BadResponseError,
+      ::HTTPClient::ConnectTimeoutError,
+      ::HTTPClient::ReceiveTimeoutError
+    ].freeze
+    NET_EXCEPTIONS = [::Net::HTTPFatalError, ::Net::HTTPServerException, ::Net::OpenTimeout].freeze
+
+    NETWORK_EXCEPTIONS = ARANHA_EXCEPTIONS + CORE_EXCEPTIONS + ERRNO_EXCEPTIONS +
+                         HTTPCLIENT_EXCEPTIONS + NET_EXCEPTIONS
+
+    class << self
+      def rescuable_error?(error)
+        return true if NETWORK_EXCEPTIONS.any? { |klass| error.is_a?(klass) }
+
+        error.cause.present? ? network_error?(error.cause) : false
+      end
+    end
+
+    enable_simple_cache
+    common_constructor :address
+
+    def successful?
+      error.blank?
+    end
+
+    def rescuable_error?
+      self.class.rescuable_error?(error)
+    end
+
+    private
+
+    def error_uncached
+      address.process
+      nil
+    rescue ::StandardError => e
+      e
+    end
+  end
+end
--- lib/aranha/processor.rb
+++ lib/aranha/processor.rb
@@ -7,19 +7,6 @@ require 'aranha/manager'
 
 module Aranha
   class Processor
-    ARANHA_EXCEPTIONS = [::Aranha::Parsers::InvalidStateException].freeze
-    CORE_EXCEPTIONS = [::SocketError].freeze
-    ERRNO_EXCEPTIONS = [Errno::ECONNREFUSED, ::Errno::ECONNRESET].freeze
-    HTTPCLIENT_EXCEPTIONS = [
-      ::HTTPClient::BadResponseError,
-      ::HTTPClient::ConnectTimeoutError,
-      ::HTTPClient::ReceiveTimeoutError
-    ].freeze
-    NET_EXCEPTIONS = [::Net::HTTPFatalError, ::Net::HTTPServerException, ::Net::OpenTimeout].freeze
-
-    NETWORK_EXCEPTIONS = ARANHA_EXCEPTIONS + CORE_EXCEPTIONS + ERRNO_EXCEPTIONS +
-                         HTTPCLIENT_EXCEPTIONS + NET_EXCEPTIONS
-
     DEFAULT_MAX_TRIES = 3
 
     attr_reader :manager
@@ -58,20 +45,20 @@ module Aranha
     def process_address(address)
       manager.log_info("Processing #{address} (Try: #{@try}/#{max_tries_s}," \
           " Unprocessed: #{unprocessed.count}/#{::Aranha::Manager.default.addresses_count})")
-      begin
-        address.process
-        @failed.delete(address.id)
-      rescue StandardError => e
-        process_exception(address, e)
+      ap = ::Aranha::AddressProcessor.new(address)
+      if ap.successful?
+        @failed.delete(ap.address.id)
+      else
+        process_exception(ap)
       end
     end
 
-    def process_exception(address, exception)
-      raise exception unless network_exception?(exception)
+    def process_exception(address_processor)
+      raise address_processor.error unless address_processor.rescuable_error?
 
-      @failed[address.id] ||= 0
-      @failed[address.id] += 1
-      manager.log_warn(exception)
+      @failed[address_processor.address.id] ||= 0
+      @failed[address_processor.address.id] += 1
+      manager.log_warn(address_processor.error)
     end
 
     def next_address
@@ -80,12 +67,6 @@ module Aranha
 
     def unprocessed
       ::Aranha::Manager.default.unprocessed_addresses
-    end
-
-    def network_exception?(exception)
-      return true if NETWORK_EXCEPTIONS.any? { |klass| exception.is_a?(klass) }
-
-      exception.cause.present? ? network_exception?(exception.cause) : false
     end
 
     def not_try_ids
