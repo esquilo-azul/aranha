d843826d4ad311d807d5f2577efbd8a8cea51abc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-26 21:53:35 -0300

    Aranha::TemporaryErrors::SELENIUM_ERRORS.

--- lib/aranha/temporary_errors.rb
+++ lib/aranha/temporary_errors.rb
@@ -4,6 +4,7 @@ require 'aranha/parsers/invalid_state_exception'
 require 'aranha/parsers/source_address/fetch_content_error'
 require 'httpclient'
 require 'eac_ruby_utils/core_ext'
+require 'selenium-webdriver'
 
 module Aranha
   module TemporaryErrors
@@ -17,9 +18,10 @@ module Aranha
       ::HTTPClient::ReceiveTimeoutError
     ].freeze
     NET_ERRORS = [::Net::HTTPFatalError, ::Net::HTTPServerException, ::Net::OpenTimeout].freeze
+    SELENIUM_ERRORS = [::Selenium::WebDriver::Error::TimeoutError].freeze
 
     ALL_ERRORS = ARANHA_ERRORS + CORE_ERRORS + ERRNO_ERRORS +
-                 HTTPCLIENT_ERRORS + NET_ERRORS
+                 HTTPCLIENT_ERRORS + NET_ERRORS + SELENIUM_ERRORS
 
     class << self
       def errors
