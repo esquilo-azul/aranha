c5fd08906f0e0dd409c4082c92c008a3072ef946
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-19 15:45:44 -0300

    Aranha::TemporaryErrors::SELENIUM_ERRORS: convert to "aranha-selenium"#Aranha::Selenium::TemporaryErrors.

--- lib/aranha/temporary_errors.rb
+++ lib/aranha/temporary_errors.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-require 'selenium-webdriver'
-
 module Aranha
   module TemporaryErrors
     CORE_ERRORS = [::SocketError].freeze
     ERRNO_ERRORS = [Errno::ECONNREFUSED, ::Errno::ECONNRESET].freeze
     NET_ERRORS = [::Net::HTTPFatalError, Net::HTTPClientException, ::Net::OpenTimeout].freeze
-    SELENIUM_ERRORS = [::Selenium::WebDriver::Error::TimeoutError].freeze
 
-    ALL_ERRORS = CORE_ERRORS + ERRNO_ERRORS + NET_ERRORS + SELENIUM_ERRORS
+    ALL_ERRORS = CORE_ERRORS + ERRNO_ERRORS + NET_ERRORS
 
     class << self
       def errors
